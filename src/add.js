7bb2c2a58ee141cc1bf985b60fbaf63f43b6a62e
Jared <jared@Michaels-iMac.local>
2018-05-16 17:32:32 -0400

    cleaned up add.js

--- src/add.js
+++ src/add.js
@@ -3,10 +3,8 @@ const path = require('path')
 const remote = electron.remote
 const closeBtn = document.getElementById('closeBtn')
 
-console.log("outside");
 
 closeBtn.addEventListener('click', function (event) {
-	console.log("clicked!");
-    var window1 = remote.getCurrentWindow();
-    window1.close();
+    var window = remote.getCurrentWindow();
+    window.close();
 })
